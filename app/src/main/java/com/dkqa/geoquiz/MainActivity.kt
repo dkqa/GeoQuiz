d739393480d56a1044c64bca306fb1659a47b15a
dkqa <dkqmoto@gmail.com>
2022-01-09 16:19:37 +0300

    Exercise Chapter 2 - MVC

--- app/src/main/java/com/dkqa/geoquiz/MainActivity.kt
+++ app/src/main/java/com/dkqa/geoquiz/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -43,8 +47,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            nextQuestion()
+        }
+
+        prevButton.setOnClickListener {
+            previousQuestion()
+        }
+
+        questionTextView.setOnClickListener {
+            nextQuestion()
         }
 
         updateQuestion()
@@ -65,5 +76,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateQuestion()
+    }
+
+    private fun previousQuestion() {
+        currentIndex = abs((currentIndex - 1) % questionBank.size)
+        updateQuestion()
     }
 }
